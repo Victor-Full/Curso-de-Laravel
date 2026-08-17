79a4ae91129a6108bc4c284a732631bd291659b4
José Victor de J. Pascoal <jose.pascoal@cgm.pms.ad>
2026-08-17 11:37:29 -0300

    aula6

---
+++ app/Http/Controllers/Site/SiterController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+class SiterController
+{
+    public function contact()
+    {
+        return view('site/contact');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
-
+use App\Http\Controllers\Site\SiterController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/contato', [SiterController::class, 'contact']);
 
 Route::get('/', function () {
     return view('welcome');
