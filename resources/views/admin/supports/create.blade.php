927952557cd55dacf544082974e4af40adec1556
José Victor de J. Pascoal <jose.pascoal@cgm.pms.ad>
2026-08-18 16:44:57 -0300

    aula 11

---
+++ resources/views/admin/supports/create.blade.php
@@ -0,0 +1,7 @@
+<h1> Nova Dúvida</h1>
+
+<from action="" method="POST">
+    <input type="text" placeholder="Assunto" name="subject">
+    <textarea name="body" cols="30" rows="5" placeholder="Descrição"></textarea>
+    <button type="submit">Enviar</button>
+</from>
